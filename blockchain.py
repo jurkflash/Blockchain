79fa11048b0bc6100a8cb2b3c8e066104face9b9
Yik Wei <jurkflash@hotmail.com>
2024-10-11 22:06:48 +0800

    Add file access

--- blockchain.py
+++ blockchain.py
@@ -24,6 +24,24 @@ owner = 'Max'
 # Registered participants: Ourself + other people sending/ receiving coins
 participants = {'Max'}
 
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+
+load_data()
+
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(str(blockchain))
+        f.write('\n')
+        f.write(str(open_transactions))
+
 
 def valid_proof(transactions, last_hash, proof):
     """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
@@ -123,6 +141,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -153,6 +172,7 @@ def mine_block():
         'proof': proof
     }
     blockchain.append(block)
+    save_data()
     return True
 
 
