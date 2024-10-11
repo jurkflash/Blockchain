316e37425bbdb5f328aa9fb1b3f67ec0427d7e13
Yik Wei <jurkflash@hotmail.com>
2024-10-11 22:16:12 +0800

    OOP - Print class with special method

--- OOP/car.py
+++ OOP/car.py
@@ -5,14 +5,21 @@ class Car:
         self.top_speed = starting_top_speed
         self.warnings = []
 
+    def __repr__(self):
+        print('Printing...')
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
+
 
 car1 = Car()
 car1.drive()
 
 # Car.top_speed = 200
 car1.warnings.append('New warning')
+# print(car1.__dict__)
+print(car1)
 
 car2 = Car(200)
 car2.drive()
